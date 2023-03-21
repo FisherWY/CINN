21ecbf8c09ff8c0fc9ea57abeb5be8e4c90d0176
Fisher <fisheryung@outlook.com>
2023-03-21 11:00:02 +0000

    Add op mapper floor_divide

---
+++ cinn/frontend/op_mappers/paddle/floor_divide.cc
@@ -0,0 +1,46 @@
+// Copyright (c) 2023 CINN Authors. All Rights Reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "cinn/frontend/op_mapper_registry.h"
+#include "cinn/frontend/op_mappers/common_utils.h"
+
+namespace cinn {
+namespace frontend {
+namespace paddle_mappers {
+
+void FloorDivideOpMapper(const paddle::cpp::OpDesc& op_desc, const OpMapperContext& ctx) {
+  CHECK_EQ(op_desc.Input("x").size(), 1UL);
+  auto x_name = op_desc.Input("x").front();
+  CHECK_EQ(op_desc.Input("y").size(), 1UL);
+  auto y_name = op_desc.Input("y").front();
+  CHECK_EQ(op_desc.Output("out").size(), 1UL);
+  auto out_name = op_desc.Output("out").front();
+
+  auto x = ctx.GetVar(x_name);
+  auto y = ctx.GetVar(y_name);
+
+  auto out = ctx.Builder()->FloorDivide(x, y);
+
+  ctx.AddVar(out_name, out);
+  ctx.AddVarModelToProgram(out_name, out->id);
+}
+
+}  // namespace paddle_mappers
+}  // namespace frontend
+}  // namespace cinn
+
+CINN_REGISTER_HELPER(paddle_floor_divide) {
+  CINN_REGISTER_OP_MAPPER(floor_divide, cinn::frontend::paddle_mappers::FloorDivideOpMapper)
+  return true;
+}
